9103ba64fd4ef29770294442e9af4e05d06a4152
Cuong23122001 <cuongnmgch190696@fpt.edu.vn>
2022-11-22 14:50:51 +0700

    ok

--- app.js
+++ app.js
@@ -1,51 +1,92 @@
 const express = require('express')
 const app = express()
-const { insertObject, getRole, getDB } = require('./databaseHandler')
+const { getDB } = require('./databaseHandler')
 const { ObjectId } = require('mongodb')
 const async = require('hbs/lib/async')
 
-app.set('view engine','hbs')
-app.use(express.urlencoded({extended:true}));
+app.set('view engine', 'hbs')
+app.use(express.urlencoded({ extended: true }));
 
 
-app.post('/sendPayLoad', (req,res)=>{
+app.post('/sendPayLoad', (req, res) => {
     let response = ''
     try {
         const payLoad = req.body.jsonpayload
-        console.log(payLoad)
+        console.log("payLoad " + payLoad)
+
         const jsonObjec = JSON.parse(payLoad)
-        
+
         console.log(jsonObjec.userId)
+
         console.log(jsonObjec.detailList.length)
+
         let names = ''
+        let destinations = ''
+        let dates = ''
+        let descriptions = ''
+        let transportions = ''
+        let numbers = ''
+        let risk = ''
         jsonObjec.detailList.forEach(element => {
             names += element.name + ","
+            destinations += element.destination + ","
+            dates += element.date + ","
+            descriptions += element.description + ","
+            transportions += element.modeOfTransportation + ","
+            numbers += element.amountOfPeople + ","
+            risk += element.riskAssessment + ","
         });
-        names = names.substring(0,names.length -1) // remove the last ","
+        console.log("names " + names);
+        names = names.substring(0, names.length - 1) // remove the last ","
+
         console.log(jsonObjec.detailList[0].name)
+        console.log("names " + names);
         response = {
-            'uploadResponseCode' :'SUCCESS',
-            'userid' : jsonObjec.userId,
+            'uploadResponseCode': 'SUCCESS',
+            'userid': jsonObjec.userId,
             'number': jsonObjec.detailList.length,
-            'names' : names,
-            'message':'successful upload – all done!'
+            'name': names,
+            'destination': destinations,
+            'date': dates,
+            'description':descriptions,
+            'mode of transportation': transportions,
+            'amount of people': numbers,
+            'risk assessment': risk,
+            'message': 'successful upload – all done!'
 
         }
     } catch (error) {
         response = {
-            'uploadResponseCode' :'ERROR',
-            'message':'your request is invalid! check the request format!'
+            'uploadResponseCode': 'ERROR',
+            'message': 'your request is invalid! check the request format!'
         }
-    }    
+    }
     res.json(response)
 })
-app.get('/',async(req,res)=>{
+app.get('/', async (req, res) => {
+    const allExpense = []
+
     const db = await getDB();
     const a = await db.collection('DataWebservice').find({}).toArray();
-    console.log(a);
-    res.render('webservice')
+    
+    for (let i = 0; i < a.length; i++) {
+            await db.collection('DataWebservice').updateOne({name: a[i].name},{
+                $set:{
+                    allExpense:allExpense
+                }
+            })
+        }
+    for (let i = 0; i < a.length; i++) {
+        const trip = a[i].name;
+        const expense = await db.collection('DataExpenseWebservice').find({ name_trip: trip }).toArray();
+        await db.collection('DataWebservice').updateOne({name:trip},{
+            $set:{
+                allExpense:expense
+            }
+        })
+    }
+    res.render('webservice',{data:a})
 })
-
-const PORT = process.env.PORT || 5500
+const PORT = process.env.PORT || 5111
 app.listen(PORT)
 console.log("Server is running! " + PORT)
--- databaseHandler.js
+++ databaseHandler.js
@@ -1,8 +1,9 @@
 const async = require('hbs/lib/async');
-const { MongoClient, ObjectId } = require('mongodb');
+const { MongoClient } = require('mongodb');
 
 // const URL = 'mongodb://0.0.0.0:27017';
-const URL = 'mongodb+srv://admin:hsPOvEloe0djkzKP@cluster0.zcq0f.mongodb.net/test'
+// const URL = 'mongodb+srv://admin:hsPOvEloe0djkzKP@cluster0.zcq0f.mongodb.net/test'
+const URL = 'mongodb+srv://tripmanagementapp:asdase2312easasfa@cluster0.jipi4x0.mongodb.net/test'
 const DATABASE_NAME = "AppMobile"
 
 async function getDB() {
@@ -10,55 +11,6 @@ async function getDB() {
     const dbo = client.db(DATABASE_NAME);
     return dbo;
 }
-
-async function insertObject(collectionName, objectToInsert) {
-    const dbo = await getDB();
-    const newObject = await dbo.collection(collectionName).insertOne(objectToInsert);
-}
-async function deleteManager(userName) {
-    const dbo = await getDB();
-    await dbo.collection("Manager").deleteOne({ "userName": userName });
-    await dbo.collection("User").deleteOne({ "userName": userName })
-}
-async function deleteCoordinator(userName) {
-    const dbo = await getDB();
-    await dbo.collection("Coordinator").deleteOne({ "userName": userName });
-    await dbo.collection("User").deleteOne({ "userName": userName })
-}
-async function deleteStaff(userName) {
-    const dbo = await getDB();
-    await dbo.collection("Staff").deleteOne({ "userName": userName });
-    await dbo.collection("User").deleteOne({ "userName": userName })
-}
-async function deleteCategory(id) {
-    const dbo = await getDB();
-    await dbo.collection("Category").deleteOne({ "_id": ObjectId(id) });
-}
-async function deleteDepartment(id) {
-    const dbo = await getDB();
-    await dbo.collection("Department").deleteOne({ "_id": ObjectId(id) });
-}
-
-//check role to login
-async function getRole(nameI, passI) {
-    const db = await getDB()
-    const user = await db.collection("User").findOne({ userName: nameI, password: passI });
-    if (user == null) {
-        return "-1"
-    } else {
-        console.log(user);
-        return user.role
-    }
-}
-
 module.exports = {
-    getDB,
-    ObjectId,
-    insertObject,
-    deleteCoordinator,
-    deleteManager,
-    deleteStaff,
-    deleteCategory,
-    getRole,
-    deleteDepartment
+    getDB
 }
